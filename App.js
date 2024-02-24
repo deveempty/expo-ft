4d6bd277c61025e286ad4b2188e16c4f4e6b24d3
Juan Viloria <juanviloriav@gmail.com>
2024-02-24 15:32:36 -0600

    Button created as component

--- App.js
+++ App.js
@@ -5,12 +5,17 @@ const background = require("./assets/images/background-image.png")
 
 // Components
 import ImageView from './src/components/ImageView/ImagenView';
+import Button from './src/components/Button/Button';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageView img={background}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label={"Hello"}/>
+        <Button label={"Hello"}/>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -20,12 +25,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
   imageContainer : {
     flex: 1,
     paddingTop: 58
-  }
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
